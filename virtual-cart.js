64c601587ff0f0a25549443009ec20f4289bfec1
Harsh Pandey <hpandey793@gmail.com>
2015-10-17 13:42:24 -0700

    initial commit

---
+++ virtual-cart.js
@@ -0,0 +1,39 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
+if (Meteor.isCordova) {
+  Meteor.startup(function () {
+    cordova.plugins.barcodeScanner.scan(
+      function (result) {
+        alert("We got a barcode\n" +
+          "Result: " + result.text + "\n" +
+          "Format: " + result.format + "\n" +
+          "Cancelled: " + result.cancelled);
+      }, 
+      function (error) {
+        alert("Scanning failed: " + error);
+      }
+    );
+  });
+}
