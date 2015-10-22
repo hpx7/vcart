3fffee073968b2f0dc1a8d1ca08e14c7a067979c
Varun Murali <varunm@uber.com>
2015-10-21 23:57:18 -0700

    scan button on cart

--- client/app.js
+++ client/app.js
@@ -1,15 +1,5 @@
 if (Meteor.isCordova) {
   Meteor.startup(function () {
-    cordova.plugins.barcodeScanner.scan(
-      function (result) {
-        alert("We got a barcode\n" +
-          "Result: " + result.text + "\n" +
-          "Format: " + result.format + "\n" +
-          "Cancelled: " + result.cancelled);
-      }, 
-      function (error) {
-        alert("Scanning failed: " + error);
-      }
-    );
+    
   });
 }
